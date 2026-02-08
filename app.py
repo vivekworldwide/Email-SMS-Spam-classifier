25250cfa7c2ee5ddaa62227ffb1ec9f6d3295a61
vivekworldwide <vivek.baghel4149@gmail.com>
2026-02-08 18:22:35 +0530

    steamlit move

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pickle
 import string
 import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 ps = PorterStemmer()
